7a18d8c72724007685d2ddc52ef9b86626a894db
dmitry131131 <antontsev.dmitry@yandex.ru>
2023-09-16 11:40:47 +0300

    Made documentate comments

--- include/InputOutput.h
+++ include/InputOutput.h
@@ -10,13 +10,30 @@ struct textData {
     char*  bufferName;  ///< Name of buffer
     char** linesPtr;    ///< Name of lines array
 };
-
+/**
+ * @brief Function find size of file
+ * @param [in] file pointer to file
+ * @return size of file
+*/
 size_t get_file_size(FILE* file);
-
+/**
+ * @brief Input text from file
+ * @param [in] file     pointer to file
+ * @param [in] fileSize size of file
+ * @return pointer to text buffer
+*/
 char* get_file(FILE* file, size_t fileSize);
-
+/**
+ * @brief Function counts number of occurrences ch in buffer
+ * @param [in] buffer text line
+ * @param [in] ch     char
+ * @return number of occurrences ch in buffer
+*/
 size_t char_count(const char* buffer, char ch);
-
+/**
+ * @brief function cut buffer on lines
+ * @param [in,out] text text struct
+*/
 int get_lines(struct textData* text);
 
 int remove_text(struct textData* text);
--- include/Sorts.h
+++ include/Sorts.h
@@ -9,15 +9,42 @@
  * @param [in] secondStr Second string
 */
 int cmp_strings(const void* firstStr, const void* secondStr);
-
+/**
+ * @brief choice strings sort algoritm
+ * @param [in,out] array sorted array
+ * @param [in]     count count of lines
+*/
 void choice_sort(char** array, size_t count);
-
+/**
+ * @brief function find minimal element in string array
+ * @param [in] array array where we fing min element
+ * @param [in] size  size of array
+ * @return pointer to min string
+ */
 char** min_string(char** array, size_t size);
-
+/**
+ * @brief Function reverse symbol array
+ * @param [in,out] string Symbol array
+ * @return pointer to first symbol of reversed symbol
+*/
 char* str_reverse(char* string);
-
+/**
+ * @brief Function compare two reversed strings
+ * @param [in] s1 first string
+ * @param [in] s2 second string
+ * @return 0 if s1 == s2, >0 if s1 > s2, <0 if s1 < s2 
+*/
 int strcmp_reverse(const char* s1, const char* s2);
-
+/**
+ * @brief finction find minimal string in reverse strings array
+ * @param [in] array lines array
+ * @param [in] size  size of array
+ * @return pointer to min string
+*/
 char** min_string_reverse(char** array, size_t size);
-
+/**
+ * @brief choice reverse strings sort algoritm
+ * @param [in,out] array sorted array
+ * @param [in]     count count of lines
+*/
 void choice_sort_reverse(char** array, size_t count);
